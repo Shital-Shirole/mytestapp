5de95ccee6bfd4380aa67345dd36d7cf423b0095
Shital Shirole <shitalshirole89@gmail.com>
2020-12-10 17:20:59 +0530

    added few lines

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,9 @@
 public class HelloWorld {
    public static void main(String []args) {
       System.out.println("Hello World in app");
+      System.out.println("testing1");
+      System.out.println("testing2");
+      System.out.println("testing3");
+      System.out.println("testing4");
    }
 }
